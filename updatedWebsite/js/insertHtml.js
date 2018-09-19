41e1647727accbabad2d46bd023f787edbb5d4e1
rjfaure <rjfaure@iastate.edu>
2018-09-19 15:57:02 -0500

    Removed old versions of website

--- updatedWebsite/js/insertHtml.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("header").load("../header.html");
-});
-
-$(document).ready(function(){
-  $("footer").load("../footer.html");
-});
--- updatedWebsite/js/openNav.js
+++
@@ -1,8 +0,0 @@
-function openNav(){
-  var x = document.getElementById("head-nav");
-  if(x.className === "nav-bar"){
-    x.className += " responsive";
-  }else{
-    x.className = "nav-bar";
-  }
-}
